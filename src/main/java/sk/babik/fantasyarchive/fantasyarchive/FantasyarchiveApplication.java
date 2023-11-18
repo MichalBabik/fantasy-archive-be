e3efedbe9444b0ed5b0da48afa7f6783dc4294a4
MichalBabik <babik@stud.uniza.sk>
2023-11-18 19:32:58 +0100

    initial commit

---
+++ src/main/java/sk/babik/fantasyarchive/fantasyarchive/FantasyarchiveApplication.java
@@ -0,0 +1,13 @@
+package sk.babik.fantasyarchive.fantasyarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyarchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/babik/fantasyarchive/fantasyarchive/FantasyarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.babik.fantasyarchive.fantasyarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
